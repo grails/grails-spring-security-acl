4b7deef0e7329320c81b79cd44f185b0699555e6
Burt Beckwith <beckwithb@vmware.com>
2012-08-20 16:17:02 -0400

    update to Grails 2.0.4, latest release plugin, and spring-security-acl 3.0.7 from Maven Central instead of ebr

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,27 +1,35 @@
-grails.project.class.dir = 'target/classes'
-grails.project.test.class.dir = 'target/test-classes'
-grails.project.test.reports.dir	= 'target/test-reports'
-grails.project.docs.output.dir = 'docs' // for the gh-pages branch
+grails.project.work.dir = 'target'
+grails.project.docs.output.dir = 'docs/manual' // for the gh-pages branch
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
-	inherits('global') {
-		excludes 'commons-codec' // Grails ships with 1.3, need 1.4
-	}
-
+	inherits 'global'
 	log 'warn'
 
-	repositories {        
-		grailsPlugins()
-		grailsHome()
+	repositories {
 		grailsCentral()
-
-		ebr() // SpringSource  http://www.springsource.com/repository
+		mavenLocal()
+		mavenCentral()
 	}
 
 	dependencies {
-		runtime('org.springframework.security:org.springframework.security.acls:3.0.4.RELEASE') {
-			transitive = false
+		compile('org.springframework.security:spring-security-acl:3.0.7.RELEASE') {
+			excludes 'spring-security-core', 'spring-context-support', 'spring-jdbc',
+			         'spring-test', 'ehcache', 'hsqldb', 'postgresql', 'junit',
+			         'mockito-core', 'jmock-junit4'
+		}
+	}
+
+	plugins {
+		compile ':spring-security-core:1.2.7.3'
+
+		compile(":hibernate:$grailsVersion") {
+			export = false
+		}
+
+		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
+			export = false
 		}
 	}
 }
