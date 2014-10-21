ff42a34591f63cf150a1a44632cee22f3e949f8a
William Drai <william.drai@graniteds.org>
2014-10-21 18:29:09 +0200

    Upgrade to Grails 2.4

--- SpringSecurityAclGrailsPlugin.groovy
+++ SpringSecurityAclGrailsPlugin.groovy
@@ -60,7 +60,7 @@ import org.springframework.security.core.authority.AuthorityUtils
  */
 class SpringSecurityAclGrailsPlugin {
 
-	String version = '2.0-RC1'
+	String version = '2.0-SNAPSHOT'
 	String grailsVersion = '2.0 > *'
 	List loadAfter = ['springSecurityCore']
 	List pluginExcludes = [
