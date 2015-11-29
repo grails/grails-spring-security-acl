a3edc0af64c3f31a7f9189c37326bda12f7fb0c1
Burt Beckwith <burt@burtbeckwith.com>
2015-11-29 15:33:57 -0500

    updated controllerAnnotations.staticRules to the new format

--- functional-test-app/grails-app/conf/application.groovy
+++ functional-test-app/grails-app/conf/application.groovy
@@ -3,16 +3,16 @@ grails {
 		springsecurity {
 
 			controllerAnnotations.staticRules = [
-				'/':                ['permitAll'],
-				'/error':           ['permitAll'],
-				'/index':           ['permitAll'],
-				'/index.gsp':       ['permitAll'],
-				'/shutdown':        ['permitAll'],
-				'/assets/**':       ['permitAll'],
-				'/**/js/**':        ['permitAll'],
-				'/**/css/**':       ['permitAll'],
-				'/**/images/**':    ['permitAll'],
-				'/**/favicon.ico':  ['permitAll']
+				[pattern: '/',                 access: 'permitAll'],
+				[pattern: '/error',            access: 'permitAll'],
+				[pattern: '/index',            access: 'permitAll'],
+				[pattern: '/index.gsp',        access: 'permitAll'],
+				[pattern: '/shutdown',         access: 'permitAll'],
+				[pattern: '/assets/**',        access: 'permitAll'],
+				[pattern: '/**/js/**',         access: 'permitAll'],
+				[pattern: '/**/css/**',        access: 'permitAll'],
+				[pattern: '/**/images/**',     access: 'permitAll'],
+				[pattern: '/**/favicon.ico',   access: 'permitAll']
 			]
 
 			authority.className = 'com.testacl.Role'
