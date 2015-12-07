b24f48080020a8276355a3b6ec2a3378fba81a49
Burt Beckwith <burt@burtbeckwith.com>
2015-12-07 18:23:22 -0500

    2.0.0 release

--- SpringSecurityAclGrailsPlugin.groovy
+++ SpringSecurityAclGrailsPlugin.groovy
@@ -61,7 +61,7 @@ import org.springframework.security.core.authority.AuthorityUtils
  * @author <a href='mailto:burt@burtbeckwith.com'>Burt Beckwith</a>
  */
 class SpringSecurityAclGrailsPlugin {
-	String version = '2.0-RC3-SNAPSHOT'
+	String version = '2.0.0'
 	String grailsVersion = '2.3 > *'
 	List loadAfter = ['springSecurityCore']
 	List pluginExcludes = [
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,13 +24,13 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		compile ':spring-security-core:2.0-RC4'
+		compile ':spring-security-core:2.0.0'
 
 		compile ':hibernate:3.6.10.17', {
 			export = false
 		}
 
-		build ':release:3.1.1', ':rest-client-builder:2.1.1', {
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
 	}
