61749149b98b90dae356ea73287cc6b780fc2d13
jameskleeh <james.kleeh@gmail.com>
2016-12-12 14:19:09 -0500

    Update build to update gh-pages correctly on a tag

--- travis-build.sh
+++ travis-build.sh
@@ -2,7 +2,6 @@
 set -e
 rm -rf *.zip
 
-# all this is doing is compiling, work in progres...
 ./gradlew clean compileGroovy
 
 EXIT_STATUS=0
@@ -30,29 +29,26 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
   if [[ $TRAVIS_BRANCH == 'master' ]]; then
      rm -rf snapshot
      mkdir -p snapshot
-     cp -r ../build/docs/. ./snapshot/
+     cp -r ../build/docs/index.html ./snapshot/
+     cp -r ../build/docs/*.epub ./snapshot/
+     cp -r ../build/docs/*.pdf ./snapshot/
 
      git add snapshot/*
   fi
 
   # If there is a tag present then this becomes the latest
   if [[ -n $TRAVIS_TAG ]]; then
-      mkdir -p latest
-      cp -r ../build/docs/. ./latest/
-      git add latest/*
 
-      version="$TRAVIS_TAG"
-      version=${version:1}
-      majorVersion=${version:0:4}
-      majorVersion="${majorVersion}x"
+      version = ${TRAVIS_TAG:0:2}
+      mkdir -p "${version}"
 
-      mkdir -p "$version"
-      cp -r ../build/docs/. "./$version/"
+      cp -r ../build/docs/ghpages.html ./index.html
+      cp -r ../build/docs/index.html ./$version/
+      cp -r ../build/docs/*.epub ./$version/
+      cp -r ../build/docs/*.pdf ./$version/
+
+      git add index.html
       git add "$version/*"
-
-      mkdir -p "$majorVersion"
-      cp -r ../build/docs/. "./$majorVersion/"
-      git add "$majorVersion/*"
 
   fi
 
