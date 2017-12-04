3863690ba718970ca7619b5910d7a4654c414112
sdelamo <sergio.delamo@softamo.com>
2017-12-04 09:59:18 +0100

    Improve docs
    - Add information about distribution, snapshots, etc

--- travis-build.sh
+++ travis-build.sh
@@ -59,6 +59,27 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST
     git add snapshot/*
   fi
 
+   # If there is a tag present then this becomes the latest
+  if [[ -n $TRAVIS_TAG ]]; then
+         git rm -rf latest/
+         mkdir -p latest
+         cp -r ../docs/build/docs/. ./latest/
+         git add latest/*
+
+        version="$TRAVIS_TAG" # eg: v3.0.1
+        version=${version:1} # 3.0.1
+        majorVersion=${version:0:4} # 3.0.
+        majorVersion="${majorVersion}x" # 3.0.x
+
+        mkdir -p "$version"
+        cp -r ../docs/build/docs/. "./$version/"
+        git add "$version/*"
+
+        git rm -rf "$majorVersion"
+        cp -r ../docs/build/docs/. "./$majorVersion/"
+        git add "$majorVersion/*"
+  fi
+
   if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' ]]; then
     git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
     git push origin HEAD
